7dec3f1d4271ab126b1dee0ecad2b3d665a28d63
Edward Pina <en.pina1@gmail.com>
2021-02-21 19:57:49 +0100

    index &styling done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # Flat.destroy
 
+puts "Cleaning database..."
+
+Flat.destroy_all
+
 Flat.create!(
   name: 'Light & Spacious Garden Flat London',
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
-  number_of_guests: 3
+  number_of_guests: 3,
+  picture: "https://images.unsplash.com/photo-1552242718-c5360894aecd?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjR8fHJvb218ZW58MHx8MHw%3D&auto=format&fit=crop&w=800&q=60"
 )
 
 Flat.create!(
@@ -20,7 +25,8 @@ Flat.create!(
   address: Faker::Address.street_address,
   description: 'Old build in the hippest part of town',
   price_per_night: 100,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture: "https://images.unsplash.com/photo-1564078516393-cf04bd966897?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fHJvb218ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
 )
 
 Flat.create!(
@@ -28,7 +34,8 @@ Flat.create!(
   address: Faker::Address.street_address,
   description: 'stinky but servicable. Bring your own furniture',
   price_per_night: 25,
-  number_of_guests: 10
+  number_of_guests: 10,
+  picture: "https://images.unsplash.com/photo-1554995207-c18c203602cb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cm9vbXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
 )
 
 Flat.create!(
@@ -36,7 +43,8 @@ Flat.create!(
   address: Faker::Address.street_address,
   description: 'Chic & cheap decor that looks expensive, faux everything',
   price_per_night: 200,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture: "https://images.unsplash.com/photo-1550581190-9c1c48d21d6c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzF8fHJvb218ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
 )
 
 puts "flats built"
